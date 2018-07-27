70df9b6a5181bfe25a306d3854405e9b3d179f90
Masahiro HIGUCHI <nicoanico@garden.local>
2018-07-27 16:25:22 +0900

    convert tab to space

--- src/pncode.h
+++ src/pncode.h
@@ -2,13 +2,13 @@
 #define _PNCODE_H_
 
 
-#define D_PRINT(f,s)		(fprintf(stderr, f, s))
+#define D_PRINT(f,s)        (fprintf(stderr, f, s))
 
-#define D_OUTPUT_FILENAME	("PN")
-#define D_OUTPUT_EXTENSION	(".dat")
+#define D_OUTPUT_FILENAME   ("PN")
+#define D_OUTPUT_EXTENSION  (".dat")
 
-#define D_PN9				(9)
-#define D_PN15				(15)
+#define D_PN9               (9)
+#define D_PN15              (15)
 
 
 #endif
